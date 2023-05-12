c96a2d25a8fe2275917efdb0d7306516832e38fd
Leonardo Jaques <jaques.projetos@outlook.com>
2023-05-12 10:29:45 -0300

    feat: create cart and cart_item

---
+++ lib/models/cart.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'cart_item.dart';
+
+class Cart with ChangeNotifier {
+  final Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  void addItem(String productId, String name, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+        productId,
+        (existingItem) => CartItem(
+          id: existingItem.id,
+          productId: productId,
+          name: existingItem.name,
+          quantity: existingItem.quantity + 1,
+          price: existingItem.price,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          productId: Random().nextDouble().toString(),
+          // productId: productId,
+          name: name,
+          quantity: 1,
+          price: price,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void clear() {
+    _items.clear();
+    notifyListeners();
+  }
+}
