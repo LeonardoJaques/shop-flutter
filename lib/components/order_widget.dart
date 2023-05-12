d3b24ab76cc1dc472627baa126d0bd4d88342adf
Leonardo Jaques <jaques.projetos@outlook.com>
2023-05-12 19:52:01 -0300

    feat: improved order_widget

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -2,26 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop_flutter/models/order.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
 
   const OrderWidget({super.key, required this.order});
 
   @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(10),
-      child: ListTile(
-        title: Text('R\$ ${order.total.toStringAsFixed(2)}'),
-        subtitle: Text(
-          DateFormat(
-            'dd/MM/yyyy hh:mm',
-          ).format(order.date),
-        ),
-        trailing: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.expand_more),
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('Soma: R\$ ${widget.order.total.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat(
+                'dd/MM/yyyy hh:mm',
+              ).format(widget.order.date),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              icon: const Icon(Icons.expand_more),
+            ),
+          ),
+          if (_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: (widget.order.products.length * 25) + 10,
+              child: ListView(
+                children: widget.order.products
+                    .map((product) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              product.name,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              " - ${product.quantity}x R\$ ${product.price}",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ],
+                        ))
+                    .toList(),
+              ),
+            )
+        ],
       ),
     );
   }
