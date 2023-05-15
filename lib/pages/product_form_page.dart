6358a1c3b7a55c653da9905ef169de933cb52a3d
Leonardo Jaques <jaques.projetos@outlook.com>
2023-05-15 20:02:06 -0300

    feat: finish form

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -14,12 +14,30 @@ class _ProductFormPageState extends State<ProductFormPage> {
   Widget build(BuildContext context) {
     final priceFocus = FocusNode();
     final descriptionFocus = FocusNode();
+    final imageURLFocus = FocusNode();
+    final ValueChanged<bool>? onFocusChange;
+
+    TextEditingController imageURLController = TextEditingController();
+    void updateImage() {
+      setState(() {});
+    }
+
+//https://cdn.pixabay.com/photo/2012/04/18/23/18/dart-38220__180.png
 
     @override
     void dispose() {
       super.dispose();
       priceFocus.dispose();
       descriptionFocus.dispose();
+
+      imageURLFocus.removeListener(updateImage);
+      imageURLFocus.dispose();
+    }
+
+    @override
+    void initState() {
+      super.initState();
+      imageURLFocus.addListener(updateImage);
     }
 
     return Scaffold(
@@ -53,6 +71,41 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 focusNode: descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'URL da Imagem'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      focusNode: imageURLFocus,
+                      controller: imageURLController,
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: const EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: imageURLController.text.isEmpty
+                        ? Text('Informe a URL')
+                        : FittedBox(
+                            child: Image.network("${imageURLController.text}"),
+                            fit: BoxFit.cover,
+                          ),
+                  )
+                ],
               ),
             ],
           )),
